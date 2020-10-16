3c6a98615a65dd6cea66053dcc1eef7d875ee630
Stefan <stefan@listrom.se>
2020-10-16 12:52:06 +0200

    Added normalize css and logos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import logo from './logo512.png';
+import logo from './images/logo192.png';
+import './normalize.css';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is a start
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <div className="header">
+                <img src={logo} className="app-logo" alt="logo" />
+                <h1 className="heading">One Place</h1>
+            </div>
+            <div className="content">
+                <div className="menu leftmenu">Stuff</div>
+                <div className="article">
+                    <p>
+                      This is a start
+                    </p>
+                </div>
+            </div>
+            <div className="footer">Copywrite One Place</div>
+        </div>
+    );
 }
 
 export default App;
